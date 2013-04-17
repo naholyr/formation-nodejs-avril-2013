fd6bf3e3c1524ee94bfab0578f96aef55ced1134
Nicolas Chambrier <naholyr@gmail.com>
2013-04-17 17:01:49 +0200

    Static server + cache

--- server.js
+++ server.js
@@ -12,14 +12,23 @@ server.on('listening', function () {
   console.log('Static server: http://localhost:' + this.address().port + '/nodejs.png');
 });
 
+var cache = {};
 function serveStaticFile (req, res) {
   var filepath = path.join(__dirname, req.url);
+  var cached = cache[filepath];
+  if (cached) {
+    res.writeHead(200, {'Content-Type': cached.type, 'Content-Length': cached.data.length});
+    res.write(cached.data);
+    res.end();
+    return;
+  }
   var type = mime.lookup(filepath);
   fs.readFile(filepath, function (err, content) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/plain'});
       res.write(String(err));
     } else {
+      cache[filepath] = {type: type, data: content};
       res.writeHead(200, {'Content-Type': type, 'Content-Length': content.length});
       res.write(content);
     }
